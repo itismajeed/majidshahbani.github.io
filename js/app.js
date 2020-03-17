5ba77101d8e2118e4d0bdd096022832ab82290f7
Majid Shahbani <majidshahbani@gmail.com>
2020-03-17 23:32:42 +0330

    app.css file deleted and png version of logo deleted, sass added

---
+++ js/app.js
@@ -0,0 +1,69 @@
+var darkTheme = "css/main.dark.css";
+var lightTheme = "css/main.light.css";
+checkTheme();
+$(".main-logo").mouseover(function () {
+    var href = $("#theme-css").attr("href");
+    if (href == darkTheme) {
+        setTheme("light");
+        $(".main-logo").removeClass("rot-back");
+        $(".main-logo").removeClass("rot-fore");
+        $(".main-logo").addClass("rot-fore");
+        localStorage.setItem("theme", "light");
+    } else {
+        setTheme("dark");
+        $(".main-logo").removeClass("rot-back");
+        $(".main-logo").removeClass("rot-fore");
+        $(".main-logo").addClass("rot-back");
+        localStorage.setItem("theme","dark");
+
+    }
+});
+// $(".main-logo").mouseleave(function () {
+//     var href = $("#theme-css").attr("href");
+//     if (href == darkTheme) {
+//         $("#theme-css").attr("href", lightTheme);
+//         $(".main-logo").removeClass("rot-back");
+//         $(".main-logo").removeClass("rot-fore");
+//         $(".main-logo").addClass("rot-back");
+//         setTimeout(() => {
+
+//             $(".main-logo").addClass("rot-back");
+
+//         }, 800);
+//     } else {
+//         $("#theme-css").attr("href", darkTheme);
+//         $(".main-logo").removeClass("rot-back");
+//         $(".main-logo").removeClass("rot-fore");
+//         $(".main-logo").addClass("rot-back");
+//         setTimeout(() => {
+
+//             $(".main-logo").addClass("rot-fore");
+
+//         }, 200);
+
+//     }
+// });
+
+
+function setTheme(theme="light") {
+    if(theme=="light"){
+        $("#theme-css").attr("href", lightTheme);
+    }else{
+        $("#theme-css").attr("href", darkTheme);
+
+    }
+}
+
+
+function checkTheme() {
+    var scheme = localStorage.getItem("theme");
+    if (scheme == null) {
+        localStorage.setItem("theme", "light");
+    } else {
+        if (scheme == 'dark') {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    }
+}
